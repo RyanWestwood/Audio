efb5dc868531b612cb8f0381f6a9abee3562f560
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-05-20 02:28:36 +0100

    Added creaky floors in buildings

--- Game Audio/Assets/Work/Scripts/Sounds/Character/Clothes.cs
+++ Game Audio/Assets/Work/Scripts/Sounds/Character/Clothes.cs
@@ -14,10 +14,10 @@ public class Clothes : MonoBehaviour
         RuntimeManager.AttachInstanceToGameObject(ClothesTrack, GetComponent<Transform>(), GetComponent<Rigidbody>());
         SoundManager.PlaySound(ClothesTrack);
     }
+
     private void Update()
     {
         float volume = PlayerRigidbody.velocity.magnitude > 0.1f ? 1.0f : 0.0f;
-        Debug.Log(PlayerRigidbody.velocity.magnitude);
         ClothesTrack.setParameterByName("Volume", volume);
     }
 }
--- Game Audio/Assets/Work/Scripts/Sounds/CreakyFloor.cs
+++ Game Audio/Assets/Work/Scripts/Sounds/CreakyFloor.cs
@@ -1,18 +1,28 @@
 using FMOD.Studio;
 using FMODUnity;
+using System.Collections;
 using UnityEngine;
 public class CreakyFloor : MonoBehaviour
 {
     [SerializeField]
     private EventInstance FloorTrack;
 
-    public enum CURRENT_TERRAIN { WOOD, GRASS, STONE, SAND, MUD }; // Math these to FMOD order
-    public CURRENT_TERRAIN Terrain;
+    public enum CREAK_FREQUENCY { HIGH, MEDIUM, LOW };
+    public CREAK_FREQUENCY Frequency = CREAK_FREQUENCY.LOW;
 
     void Start()
     {
         FloorTrack = RuntimeManager.CreateInstance("event:/CreakyFloor");
         RuntimeManager.AttachInstanceToGameObject(FloorTrack, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        StartCoroutine(StartCreaks());
+    }
+
+    private IEnumerator StartCreaks()
+    {
+        FloorTrack.setParameterByName("Volume", Random.Range(0.85f, 1.0f));
         SoundManager.PlaySound(FloorTrack);
+        float randomTime = Random.Range(5.0f, 10.0f);
+        yield return new WaitForSeconds(randomTime * ((float)Frequency + 1));
+        StartCoroutine(StartCreaks());
     }
 }
